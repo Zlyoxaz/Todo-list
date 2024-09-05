cae0ea949754e59ad3764b01eead106a86c0d066
Alex <mr.alexey537@gmail.com>
2024-09-06 02:48:30 +0300

    todo+webpack0.1.1

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,11 @@ module.exports = {
   entry: './src/js/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, './src/js'),
+    path: __dirname + '/dist/js',
   },
   watch: true,
+
+  devtool: 'source-map',
+
+  module: {},
 };
